ff68cbc87228398e342bd54560bed8884b198800
Vivek Ranjan <90482266+VivekRanjan001@users.noreply.github.com>
2023-04-24 23:12:21 +0530

    Add files via upload

---
+++ Assignment_04.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Vivek_Assignment_FAI
+{
+    class Assignment_04
+    {
+        static void Main(string[] args)
+        {
+            // Get array size from user
+            Console.WriteLine("Enter the size of the array:");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            // Dynamically create array based on user input
+            int[] array = new int[size];
+
+            // Get array values from user
+            Console.WriteLine("Enter the values for the array:");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Value {i + 1}: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            // Display array contents
+            Console.WriteLine("Array contents:");
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Value {i + 1}: {array[i]}");
+            }
+
+            // Wait for user input before closing
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assignment_07.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Vivek_Assignment_FAI
+{
+    class Assignment_07
+    {
+        static bool IsPrimeNumber(int num)
+        {
+            if (num <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            // Call the isPrimeNumber function with different values
+            int[] numbers = { 2, 3, 5, 7, 11, 15, 20, 29, 30, 37 };
+
+            foreach (int num in numbers)
+            {
+                if (IsPrimeNumber(num))
+                {
+                    Console.WriteLine($"{num} is a prime number.");
+                }
+                else
+                {
+                    Console.WriteLine($"{num} is not a prime number.");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
